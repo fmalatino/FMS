dc4dd009930a11ed5dab6f93fd273b2c9550f436
Frank Malatino <frank.malatino@noaa.gov>
2024-05-01 15:43:53 -0400

    bind(c) parameter added to memutils.F90 subroutines

--- memutils/memutils.F90
+++ memutils/memutils.F90
@@ -32,6 +32,7 @@
 !> @{
 module memutils_mod
 !Author: Balaji (V.Balaji@noaa.gov)
+
   use mpp_mod, only: mpp_pe, mpp_root_pe, mpp_npes, mpp_min, mpp_max, mpp_sum, stderr
   use mpp_memutils_mod, only: mpp_print_memuse_stats
 
@@ -51,7 +52,7 @@ contains
   !! module variables when called.  This configures if print_memuse_stats() should
   !! print the memory statistics when called.  memutils_init, at present, can
   !! be called multiple times, and potentially change the behavior of print_memuse_stats().
-  subroutine memutils_init(print_flag)
+  subroutine memutils_init(print_flag) bind(c)
     logical, optional :: print_flag !< Indicate if memory statistics should be printed by default
 
     if ( PRESENT(print_flag) ) print_memory_usage = print_flag
@@ -67,7 +68,7 @@ contains
 !! default behavior can be modified passing in the optional `always` (logical)
 !! parameter.  If `always.eqv..TRUE.`, print_memuse_stats() will print the memory
 !! statistics.
-  subroutine print_memuse_stats( text, unit, always )
+  subroutine print_memuse_stats( text, unit, always ) bind(c)
     character(len=*), intent(in) :: text !< Text to be printed before the memory statistics
     integer, intent(in), optional :: unit !< Fortran file unit to where memory statistics should be recorded
     logical, intent(in), optional :: always !< If `.TRUE.`, force memory statistics to be printed
